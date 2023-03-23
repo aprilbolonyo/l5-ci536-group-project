4b69c8a1b90dbb30d64f27af55d8349cc6d0b13a
Bkleszko <126885679+Bkleszko@users.noreply.github.com>
2023-03-23 20:13:24 +0000

    Add files via upload

---
+++ website code/javascript/menu.js
@@ -0,0 +1,10 @@
+//var dropdownBtn = document.getElementById("dropdown-btn");
+//var dropdownMenu = document.getElementById("dropdown-menu");
+
+//dropdownBtn.addEventListener("click", function() {
+//  if (dropdownMenu.style.display === "none") {
+//    dropdownMenu.style.display = "block";
+//  } else {
+//    dropdownMenu.style.display = "none";
+//  }
+//});

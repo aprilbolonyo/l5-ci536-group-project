ac77d3020203fe836355ca5f19786594b0934fce
April Bolonyo <hello@aprilbolonyo.com>
2023-05-19 12:49:57 +0100

    Added search bar functionality Javascript.

--- javascript/menu.js
+++ javascript/menu.js
@@ -6,3 +6,22 @@ window.addEventListener("load", function () {
         dropdownMenu.classList.toggle("hide");
     });
 });
+
+// <!-- HOPEFULLY SCRIPT TO CREATE SEARCHBAR  -->
+
+
+    const searchBar = document.getElementById('search-bar');
+    const cards = document.querySelectorAll('.card');
+    
+    searchBar.addEventListener('keyup', () => {
+      const searchText = searchBar.value.toLowerCase();
+      cards.forEach(card => {
+        const cardText = card.textContent.toLowerCase();
+        if (cardText.includes(searchText)) {
+          card.style.display = 'block';
+        } else {
+          card.style.display = 'none';
+        }
+      });
+    });
+

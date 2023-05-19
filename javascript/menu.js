49007e01936411f07a5b464fbeb339e1af054f0f
jake <extrudenerbs@gmail.com>
2023-05-19 13:59:29 +0100

    Added relevant Javascript for sellingPage

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,27 +1,38 @@
+// Dropdown menu:
 window.addEventListener("load", function () {
-    const dropdownBtn = document.querySelector("#dropdown-btn"),
-        dropdownMenu = document.querySelector("#dropdown-menu");
+  const dropdownBtn = document.querySelector("#dropdown-btn"),
+    dropdownMenu = document.querySelector("#dropdown-menu");
 
-    dropdownBtn.addEventListener("click", function () {
-        dropdownMenu.classList.toggle("hide");
-    });
+  dropdownBtn.addEventListener("click", function () {
+    dropdownMenu.classList.toggle("hide");
+  });
 });
 
-// <!-- HOPEFULLY SCRIPT TO CREATE SEARCHBAR  -->
+// Searchbar:
+const searchBar = document.getElementById('search-bar');
+const cards = document.querySelectorAll('.card');
 
+searchBar.addEventListener('keyup', () => {
+  const searchText = searchBar.value.toLowerCase();
+  cards.forEach(card => {
+    const cardText = card.textContent.toLowerCase();
+    if (cardText.includes(searchText)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+});
 
-    const searchBar = document.getElementById('search-bar');
-    const cards = document.querySelectorAll('.card');
-    
-    searchBar.addEventListener('keyup', () => {
-      const searchText = searchBar.value.toLowerCase();
-      cards.forEach(card => {
-        const cardText = card.textContent.toLowerCase();
-        if (cardText.includes(searchText)) {
-          card.style.display = 'block';
-        } else {
-          card.style.display = 'none';
-        }
-      });
-    });
+// Image upload:
+function submitBtn() {
+  let input = document.createElement('input');
+  input.type = 'file';
+  input.onchange = _ => {
+    let files = Array.from(input.files);
+    input.log(files);
+  };
+  input.click();
+
+}
 

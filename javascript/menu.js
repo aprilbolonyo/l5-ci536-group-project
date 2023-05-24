eaf3407b853f809caa28410a471f58c3202ded9b
jake <extrudenerbs@gmail.com>
2023-05-24 13:52:53 +0100

    Added paypal buttons and script for functionality

--- javascript/menu.js
+++ javascript/menu.js
@@ -36,21 +36,5 @@ function submitBtn() {
 
 }
 
-// thanks for your order message
-function displayThankYou() {
-  var thankYouMessage = document.createElement("div");
-  thankYouMessage.className = "thank-you-message";
-  thankYouMessage.textContent = "Thanks for your order!";
-  document.body.appendChild(thankYouMessage);
 
-  setTimeout(function() {
-    document.body.removeChild(thankYouMessage);
-  }, 3000);
-}
-
-var payLaterButton = document.querySelector('input[value="Order now, pay later (Cash)"]');
-payLaterButton.addEventListener("click", function(event) {
-  event.preventDefault(); 
-  displayThankYou();
-});
 

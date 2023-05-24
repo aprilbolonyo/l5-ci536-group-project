49375ee06eedb412d67abbe4453ad3e45e6bc797
Bartosz Kleszko <bartkleszko@gmail.com>
2023-05-24 20:45:42 +0100

    I added a unique item page and made it so that the product info is passed to the page.

--- javascript/menu.js
+++ javascript/menu.js
@@ -36,5 +36,37 @@ function submitBtn() {
 
 }
 
+function transferData(idItemName, idPrice, idName, idRating, idLocation, idInfo, idPicture, idPhone) {
+  var data = idItemName + '|' + idPrice + '|' + idName + '|' + idRating + '|' + idLocation + '|' + idInfo + '|' + idPicture + '|' + idPhone;
+  window.location.href = 'itemInfo.html?data=' + encodeURIComponent(data);
+}
 
+
+window.addEventListener('DOMContentLoaded', function() {
+  var urlParams = new URLSearchParams(window.location.search);
+  var data = urlParams.get('data');
+
+  if (data) {
+    var values = data.split('|');
+    var idItemName = values[0];
+    var idPrice = values[1];
+    var idName = values[2];
+    var idRating = values[3];
+    var idLocation = values[4];
+    var idInfo = values[5];
+    var idPicture = values[6];
+    var idPhone = values[7];
+
+    // Update the headings with the values
+    document.getElementById('itemName').textContent = idItemName;
+    document.getElementById('itemPrice').textContent = idPrice;
+    document.getElementById('sellerName').textContent = idName;
+    document.getElementById('itemRating').textContent = idRating;
+    document.getElementById('itemLocation').textContent = idLocation;
+    document.getElementById('itemInfo').textContent = idInfo;
+    var itemImage = document.getElementById('itemImage');
+    itemImage.innerHTML = "<img src='" + idPicture + "'>";
+    document.getElementById('sellerPhone').textContent = idPhone;
+  }
+});
 
